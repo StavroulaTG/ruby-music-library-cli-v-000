56bbf8386a98a39600f92ccbaf2397bba6e6560f
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 18:52:48 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,47 @@
+class Song
+  extend Concerns::Findable
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist if artist
+    self.genre = genre if genre
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def self.create(name)
+    self.new(name).tap{|artist| artist.save}
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_song(self)
+  end
+
+end
