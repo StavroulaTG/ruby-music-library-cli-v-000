d5ae3b2e572c7701633550a9cab692e7d68e172c
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 20:34:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,11 @@ class Song
     self.find_by_name(name) ? self.find_by_name(name) : self.create(name)
   end
 
-  def self.new_from_filename
+  def self.new_from_filename(filename)
+  end
+
+  def self.create_from_filename(filename)
+    
   end
 
 end
