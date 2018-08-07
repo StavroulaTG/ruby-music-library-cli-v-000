8a1f605119eaf2ba2966cc58c504889ce4c5650d
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 22:03:49 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -62,5 +62,8 @@ class MusicLibraryController
   def list_songs_by_genre
     puts "Please enter the name of a genre:"
     user_input = gets.chomp
+    if genre = Genre.find_by_name(user_input)
+      genre.songs.sort_by(&:name).each.with_index(1) {|song, index| puts "#{index}. #{song.name} - #{song.genre.name}"}
+    end
   end
 end
