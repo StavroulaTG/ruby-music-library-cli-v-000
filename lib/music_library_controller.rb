018bc012155345f0b0bc88a31f47e8fb5487aa04
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 21:47:27 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -43,6 +43,6 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artists.all.sort_by(&:name).each.with_index(1) {|artist, index| puts "#{index}. #{artist.name}"}
+    Artist.all.sort_by(&:name).each.with_index(1) {|artist, index| puts "#{index}. #{artist.name}"}
   end
 end
