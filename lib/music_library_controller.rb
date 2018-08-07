f8f20db8d42da9c4880776bcd01c4cd85494a7a6
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 21:17:37 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,8 +5,6 @@ class MusicLibraryController
   end
 
   def call
-    user_input = ""
-    
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'.")
     puts "To list all of the artists in your library, enter 'list artists'.")
@@ -16,5 +14,20 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'.")
     puts "To quit, type 'exit'.")
     puts "What would you like to do?")
+
+    user_input = gets.chomp
+
+  when "list songs"
+    list_songs
+  when "list artists"
+    list_artists
+  when "list genres"
+    list_genres
+  when "list artist"
+    list_song_by_artist
+  when "list genre"
+    list_song_by_genre
+  when "play song"
+    play_song
   end
 end
