5aebd47789e66d228006a152b871e1cba3708b12
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 22:20:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -73,6 +73,6 @@ class MusicLibraryController
     if (1..Song.all.length).include?(user_input)
       song = Song.all.sort_by(&:name)[user_input - 1]
     end
-    puts "Playing #{song.name} by #{song.name}
+    puts "Playing #{song.name} by #{song.artist.name}
   end
 end
