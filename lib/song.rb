680b2b70074a0ff2ab47526ca61922e938ced6a5
Stavroula Guerrero <4stacyg@optonline.net>
2018-08-07 20:39:48 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,10 +46,10 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    new(song,artist,genre)
-    artist,song,genre = filename.chomp(".mp3").split("-")
+    artist, song, genre = filename.chomp(".mp3").split(" - ")
     artist = Artist.find_or_create_by_name(artist)
     genre = Genre.find_or_create_by_name(genre)
+    new(song, artist, genre)
   end
 
   def self.create_from_filename(filename)
